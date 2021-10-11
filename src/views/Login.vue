2eab5e3fbaa35e43a513b6b099466f491f28f11d
zouxiaobang <zouxiaobang@qq.com>
2021-10-11 23:02:52 +0800

    login ...

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,14 +2,14 @@
   <div class="login">
     <el-form ref="form" :model="form" label-width="80px" class="login-form" :rules="loginRules">
       <h3 class="title">管理系统</h3>
-      <el-form-item label="Name">
-        <el-input v-model="form.name"></el-input>
+      <el-form-item label="账号">
+        <el-input v-model="form.username"></el-input>
       </el-form-item>
-      <el-form-item label="Password">
+      <el-form-item label="密码">
         <el-input v-model="form.password" show-password></el-input>
       </el-form-item>
-      <el-form-item label="Remember">
-        <el-switch v-model="form.remember"></el-switch>
+      <el-form-item label="记住我">
+        <el-switch v-model="form.rememberMe"></el-switch>
       </el-form-item>
       <el-form-item>
         <el-button type="primary" @click="signIn" :loading="loading">
@@ -22,17 +22,20 @@
 </template>
 
 <script>
+import Cookies from "js-cookie";
+import {encrypt, decrypt} from "@/utils/encrypt";
+
 export default {
   name: "Login",
   data() {
     return {
       form: {
-        name: '',
+        username: '',
         password: '',
-        remember: false
+        rememberMe: false
       },
       loginRules: {
-        name: [
+        username: [
           {
             require: true,
             trigger: 'blur',
@@ -60,9 +63,38 @@ export default {
       immediate: true
     }
   },
+  created() {
+    this.getCache();
+  },
   methods: {
+    getCache() {
+      let username = Cookies.get('username');
+      let password = Cookies.get('password');
+      let rememberMe = Cookies.get('rememberMe');
+      this.form = {
+        username: username === undefined ? this.form.username : username,
+        password: password === undefined ? this.form.password : decrypt(password),
+        rememberMe: rememberMe === undefined ? false : Boolean(rememberMe)
+      };
+    },
     signIn() {
+      this.$refs.form.validate(valid => {
+        if (valid) {
+          this.loading = true;
+          if (this.form.rememberMe) {
+            Cookies.set('username', this.form.username, {expires: 30});
+            Cookies.set('password', this.form.password, {expires: 30});
+            Cookies.set('rememberMe', this.form.rememberMe, {expires: 30});
+          } else {
+            Cookies.remove('username');
+            Cookies.remove('password');
+            Cookies.remove('rememberMe');
+          }
 
+          // todo 调用登录接口
+
+        }
+      })
     }
   }
 }
